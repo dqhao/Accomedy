431b85ae9f4bafc3e7b044979c0b26cd7e293c7e
dqhao <dqhao.test.001@gmail.com>
2020-09-11 20:35:36 +0800

    abc

---
+++ Accomedy.WebBackend/Accomedy.WebBackend.Data/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accomedy.WebBackend.Entities;
+
+namespace Accomedy.WebBackend.Data
+{
+    public interface IUserRepository : IGenericCrudRepository<USER, string>
+    {
+    }
+}
---
+++ Accomedy.WebBackend/Accomedy.WebBackend.Data/UserRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Accomedy.WebBackend.Entities;
+using Accomedy.WebBackend.Share;
+
+namespace Accomedy.WebBackend.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        public bool Delete(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteMany(string[] keys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TPagination<USER> Find(string xmlSearchFields, PropertyInfo sortingField, SortingEnum sortingDesc, int pageNumber, int pageCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public USER Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(USER entNew)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsExisting(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public USER Update(USER entChanged)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, string> Validate(USER animal, string type)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
